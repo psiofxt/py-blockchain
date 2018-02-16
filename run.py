2e9b902e6fd4d254c10d4a61966b0fd5855c4d56
Andrew Koller <arellok@gmail.com>
2018-02-15 17:29:32 -0800

    Added run file

---
+++ run.py
@@ -0,0 +1,16 @@
+# stdlib
+from uuid import uuid4
+
+# third-party
+
+# local
+from api.blockapi import BlockAPI
+from blockchain import Blockchain
+
+
+if __name__ == '__main__':
+    node = str(uuid4()).replace('-', '')
+    blockchain = Blockchain()
+    blockchain.new_block(0)
+    api = BlockAPI(blockchain, node)
+    api.run()
