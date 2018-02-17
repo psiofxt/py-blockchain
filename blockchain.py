402fb8f376c4f82176af25c84a29c36544380f6b
Andrew Koller <arellok@gmail.com>
2018-02-16 23:15:47 -0800

    Added resolve conflicts function

--- blockchain.py
+++ blockchain.py
@@ -2,17 +2,27 @@
 import hashlib
 import json
 from time import time
+from urllib.parse import urlparse
+import requests
 
 # third-party
 
 # local
-from proof.proof import proof_of_work
+from proof.proof import proof_of_work, valid_proof
 
 
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        self.nodes = set()
+
+    def register_node(self, address):
+        """
+        TODO: Register docstring
+        """
+        parsed = urlparse(address)
+        self.nodes.add(parsed.netloc)
 
     def new_block(self, proof, previous_hash=None):
         """
@@ -53,6 +63,56 @@ class Blockchain(object):
         })
 
         return self.last_block['index'] + 1
+
+    def valid_chain(self, chain):
+        """
+        TODO: Valid chain docstring
+        """
+        last_block = chain[0]
+        current = 1
+
+        while current < len(chain):
+            block = chain[current]
+            print(f'{last_block}')
+            print(f'{block}')
+            print('\n-----------\n')
+
+            if not block['previous_hash'] == self.hash(last_block):
+                return False
+
+            if not valid_proof(last_block['proof'], block['proof']):
+                return False
+
+            last_block = block
+            current += 1
+
+        return True
+
+    def resolve_conflicts(self):
+        """
+        TODO: Resolve conflicts docstring
+        """
+        neighbors = self.nodes
+        new_chain = None
+
+        max_len = len(self.chain)
+
+        for node in neighbors:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                if length > max_len and self.valid_chain(chain):
+                    max_len = length
+                    new_chain = chain
+
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
 
     @staticmethod
     def hash(block):
